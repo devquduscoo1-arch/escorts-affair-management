67294acca4e87fbc538f40834a391e8a9d0a72a5
mimifon42-bit <devquduscoo1@gmail.com>
2026-05-12 00:48:47 +0100

    Replace Tawk.to with Smartsupp live chat widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,19 +17,18 @@ export default function RootLayout({
       <body>
         {children}
         <Script
-          id="tawk-to"
+          id="smartsupp"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/69fbca072a55c41c32d17451/1jnvoqbrg';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-              })();
+              var _smartsupp = _smartsupp || {};
+              _smartsupp.key = 'af7db9c15723b0d28cf05722e6c1e34b25974077';
+              window.smartsupp||(function(d) {
+                var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+                s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+                c.type='text/javascript';c.charset='utf-8';c.async=true;
+                c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+              })(document);
             `,
           }}
         />
